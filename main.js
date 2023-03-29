909eb96bd36c3de18e437023af62051adb229614
Cesar Pizarra <cpizarra002@gmail.com>
2023-03-29 19:23:40 +0800

    all activities

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 AOS.init();
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
+const openModal = document.getElementById("openModal");
 
 // When the user clicks the button, open the modal
-function openModal() {
+openModal.onclick = function () {
+  this.innerHTML = '<div class="loading"></div>';
+  setTimeout(() => {
+    this.innerHTML = 'View Activities'
     modal.style.display = "block";
-    modal.style.opacity = '1';
-}
+    modal.style.opacity = "1";
+  }, 1500);
+};
 
 // When the user clicks on <span> (x), close the modal
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
